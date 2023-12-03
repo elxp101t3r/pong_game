4905e5f30a21dde750a43e968ae0c0a8d795824d
elxp101t3r <eliotx10101@outlook.com>
2023-12-03 21:34:54 +0200

    right paddle

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
+GAME_MODE = True
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title('Pong')
+screen.tracer(0)
+right_paddle = Turtle()
+right_paddle.shape('square')
+right_paddle.color('white')
+right_paddle.shapesize(stretch_wid=5, stretch_len=1)
+right_paddle.penup()
+right_paddle.setposition(x=350, y=0)
 
 
+
+def go_up():
+    the_y = right_paddle.ycor() + 20
+    right_paddle.goto(right_paddle.xcor(), the_y)
+
+
+def go_down():
+    the_y = right_paddle.ycor() - 20
+    right_paddle.goto(right_paddle.xcor(), the_y)
+
+screen.listen()
+screen.onkey(go_up, 'Up')
+screen.onkey(go_down, 'Down')
+
+
+
+while GAME_MODE:
+    screen.update()
+    
+    
 screen.exitonclick()
 print('Done')
