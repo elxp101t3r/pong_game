18a2333053a85423bdf9b31335b696b38c851701
elxp101t3r <eliotx10101@outlook.com>
2023-12-05 21:46:39 +0200

    refactoring

--- left_paddle.py
+++ left_paddle.py
@@ -1,14 +1,4 @@
-from turtle import Turtle
-
-class Paddle(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.hideturtle()
-        self.color('white')
-        self.shape('square')
-        self.shapesize(stretch_wid=5, stretch_len=1)
-        self.penup()
-
+from paddles import Paddle
 
 class LeftPaddle(Paddle):
     def __init__(self):
