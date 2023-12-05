3572538ffe1985f068298959c230af134439fa12
elxp101t3r <eliotx10101@outlook.com>
2023-12-05 21:04:33 +0200

    Go to OOP

--- main.py
+++ main.py
@@ -1,38 +1,80 @@
-from turtle import Screen, Turtle
+from turtle import Turtle, Screen
+from random import choice
 
 GAME_MODE = True
-screen = Screen()
-screen.bgcolor('black')
-screen.setup(width=800, height=600)
-screen.title('Pong')
-screen.tracer(0)
-right_paddle = Turtle()
-right_paddle.shape('square')
-right_paddle.color('white')
-right_paddle.shapesize(stretch_wid=5, stretch_len=1)
-right_paddle.penup()
-right_paddle.setposition(x=350, y=0)
+
+class Window:
+    def __init__(self):
+        self.color = 'black'
+        self.screen = Screen()
+        self.screen.bgcolor(self.color)
+        self.screen.setup(width=800, height=600)
+        self.screen.title('Pong')
 
 
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color('white')
+        self.shape('circle')
+        self.shapesize(stretch_wid=1, stretch_len=1)
+        self.penup()
+        
+          
+class Paddle(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.color('white')
+        self.shape('square')
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
 
-def go_up():
-    the_y = right_paddle.ycor() + 20
-    right_paddle.goto(right_paddle.xcor(), the_y)
+
+class RightPaddle(Paddle):
+    def __init__(self):
+        super().__init__()
+        self.goto(x=350, y=0)  
+        self.showturtle()
+    
+    def go_up(self):
+        self.the_y = self.ycor() + 20
+        self.goto(self.xcor(), self.the_y)
+    
+    def go_down(self):
+        self.the_y = self.ycor() - 20
+        self.goto(self.xcor(), self.the_y)
+    
 
 
-def go_down():
-    the_y = right_paddle.ycor() - 20
-    right_paddle.goto(right_paddle.xcor(), the_y)
+class LeftPaddle(Paddle):
+    def __init__(self):
+        super().__init__()
+        self.goto(x=-350, y=0)
+        self.showturtle()
+        self.r_move = [self.go_up, self.go_down]
+    def go_up(self):
+        self.the_y = self.ycor() + 20
+        self.goto(self.xcor(), self.the_y)
+    
+    def go_down(self):
+        self.the_y = self.ycor() - 20
+        self.goto(self.xcor(), self.the_y)
+       
+        
+window = Window()
+window.screen.listen()
+left = LeftPaddle()
+ball = Ball()
+right = RightPaddle()
 
-screen.listen()
-screen.onkey(go_up, 'Up')
-screen.onkey(go_down, 'Down')
-
-
+window.screen.onkey(right.go_up, 'Up')
+window.screen.onkey(right.go_down, 'Down')
+window.screen.onkeypress(choice(left.r_move))
 
 while GAME_MODE:
-    screen.update()
+    window.screen.update()
     
-    
-screen.exitonclick()
-print('Done')
+
+window.screen.exitonclick()
+       
